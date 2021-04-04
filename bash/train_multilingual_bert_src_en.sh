594d6e3d5b124f8c382db78b31dad4564fb2031e
subhadarship <subhadarshipanda08@gmail.com>
2021-04-04 00:50:43 -0400

    renamed expt names

--- bash/train_multilingual_bert_src_en.sh
+++ bash/train_multilingual_bert_src_en.sh
@@ -18,32 +18,39 @@ for TRGLANG in bg ar; do
   for HIDDIM in 128 256 512; do
     for LR in 0.0005 0.005 0.05; do
       # zero
-      expt=${TRGLANG}/${MODELNAME}/zero_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/zero_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # few 50
-      expt=${TRGLANG}/${MODELNAME}/few50_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/few50_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_50 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # few 100
-      expt=${TRGLANG}/${MODELNAME}/few100_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/few100_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_100 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # few 150
-      expt=${TRGLANG}/${MODELNAME}/few150_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/few150_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_150 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # few 200
-      expt=${TRGLANG}/${MODELNAME}/few200_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/few200_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_200 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # full
-      expt=${TRGLANG}/${MODELNAME}/full_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/full_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
       # trg
-      expt=${TRGLANG}/${MODELNAME}/trg_fc${HIDDIM}_lr${LR}_trainable
+      expt=en${TRGLANG}/${MODELNAME}/trg_fc${HIDDIM}_lr${LR}_trainable
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples ${TRGLANG}_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10
+      rm -rf ${MODELDIR}/${expt}
 
     done
 
@@ -56,32 +63,39 @@ for TRGLANG in bg ar; do
   for HIDDIM in 128 256 512; do
     for LR in 0.0005 0.005 0.05; do
       # zero
-      expt=${TRGLANG}/${MODELNAME}/zero_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/zero_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # few 50
-      expt=${TRGLANG}/${MODELNAME}/few50_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/few50_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_50 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # few 100
-      expt=${TRGLANG}/${MODELNAME}/few100_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/few100_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_100 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # few 150
-      expt=${TRGLANG}/${MODELNAME}/few150_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/few150_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_150 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # few 200
-      expt=${TRGLANG}/${MODELNAME}/few200_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/few200_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_200 --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # full
-      expt=${TRGLANG}/${MODELNAME}/full_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/full_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples en_all,${TRGLANG}_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
       # trg
-      expt=${TRGLANG}/${MODELNAME}/trg_fc${HIDDIM}_lr${LR}_frozen
+      expt=en${TRGLANG}/${MODELNAME}/trg_fc${HIDDIM}_lr${LR}_frozen
       CUDA_VISIBLE_DEVICES=${GPUID} python train.py --srclangs_with_num_samples ${TRGLANG}_all --trglang ${TRGLANG} --tokenization ${MODELNAME} --model_name ${MODELNAME} --bert_fc_dim ${HIDDIM} --model_dir ${MODELDIR}/${expt} --log_file_path ../logs/${expt}.txt --lr ${LR} --batch_size 1024 --max_epochs 999 --early_stopping_patience 10 --freeze_bert
+      rm -rf ${MODELDIR}/${expt}
 
     done
 
